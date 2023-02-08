060cf878503e7cfbdb2f4eac08b028b4e7e2642a
Gian-Luca Frei <gianlucafrei@gmail.com>
2023-02-08 16:42:10 +0800

    Added new ssrf challenge

--- routes/fileServer.ts
+++ routes/fileServer.ts
@@ -12,18 +12,43 @@ const security = require('../lib/insecurity')
 const challenges = require('../data/datacache').challenges
 
 module.exports = function servePublicFiles () {
-  return ({ params, query }: Request, res: Response, next: NextFunction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+
+    const params = req.params;
     const file = params.file
 
     if (!file.includes('/')) {
-      verify(file, res, next)
+      verify(file, res, req, next)
     } else {
       res.status(403)
       next(new Error('File names cannot contain forward slashes!'))
     }
   }
 
-  function verify (file: string, res: Response, next: NextFunction) {
+  function verify (file: string, res: Response, req: Request, next: NextFunction) {
+    
+    // ssrfSecretsChallenge
+    if(file && file.includes("password-list.png")){
+
+      const ip = req.socket.remoteAddress;
+      if(ip == "::1" || ip=="0.0.0.0" || ip?.includes("127.0.0.1") || ip?.includes("172.17.0.1")){
+
+        // Solve challenge only if the User-Agent is JuiceShop. This is not a security feature, but simply to prevent that
+        // users solve the challenge by accidence if the shop is running on localhost
+        if(req.get('User-Agent') == "JuiceShop"){
+          challengeUtils.solveIf(challenges.ssrfSecretsChallenge, () => { return true})
+          res.sendFile(path.resolve('ftp/', file))
+          return
+        }
+      }
+      else{
+        // Only allow localhost
+        res.status(403)
+        next(new Error('Only requests from localhost can access this secret file! Your IP is: ' + ip))
+        return;
+      }
+    }
+    
     if (file && (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
       file = security.cutOffPoisonNullByte(file)
 
--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -14,29 +14,49 @@ const logger = require('../lib/logger')
 
 module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (req.body.imageUrl !== undefined) {
-      const url = req.body.imageUrl
-      if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
-      const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
-      if (loggedInUser) {
-        const imageRequest = request
-          .get(url)
-          .on('error', function (err: unknown) {
+
+    if(req.body.imageUrl == undefined){
+      next(new Error('Blocked illegal activity by ' + req.connection.remoteAddress));
+      return;
+    }
+
+    if(invalid_url(req.body.imageUrl)){
+      next(new Error('SSRF protection, address must not start with 127.0.0.1 or localhost'));
+      return;
+    }
+
+    const url = req.body.imageUrl
+    if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
+    const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
+    if (loggedInUser) {
+
+      const options = {
+        url: url,
+        headers: {
+          'User-Agent': 'JuiceShop' // This is needed to verify the ssrf challenge
+        }
+      };
+
+      const imageRequest = request
+        .get(options)
+        .on('error', function (err: unknown) {
             UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
-            logger.warn(`Error retrieving user profile image: ${utils.getErrorMessage(err)}; using image link directly`)
-          })
-          .on('response', function (res: Response) {
-            if (res.statusCode === 200) {
-              const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
-              imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
+          logger.warn(`Error retrieving user profile image: ${utils.getErrorMessage(err)}; using image link directly`)
+        })
+        .on('response', function (res: Response) {
+          if (res.statusCode === 200) {
+            const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
+            imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
               UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` }) }).catch((error: Error) => { next(error) })
             } else UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
-          })
-      } else {
-        next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
-      }
+        })
     }
     res.location(process.env.BASE_PATH + '/profile')
     res.redirect(process.env.BASE_PATH + '/profile')
   }
+}
+
+function invalid_url(url: string): boolean {  // broken ssrf check
+  const bad_urls: string[] = ['http://127.0.0.1', 'https://127.0.0.1', 'http://localhost', 'https://localhost'];
+  return bad_urls.some(prefix => url.startsWith(prefix));
 }
